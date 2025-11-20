6cadbb11b8e688cbe503c149354942c821f078a5
Gabriel <134400952+GabzinhoHD@users.noreply.github.com>
2025-11-20 20:55:30 -0300

    exercicios completos

--- (Aula-04)ExerciciosBash/ex9.sh
+++ (Aula-04)ExerciciosBash/ex9.sh
@@ -3,11 +3,11 @@ echo "============================"
 read -p "Digite seu peso: " peso
 read -p "Digite sua altura: " altura
 
-imc=$(echo "$peso / ($altura^2)" | bc -l)
+imc=$(echo "scale=2; $peso / ($altura^2)" | bc -l)
 
 if (( $(echo "$imc < 18.5" | bc -l) )); then
     status="Abaixo do peso"
-elif (( $(echo "$imc => 18.5 && $imc <= 24.9" | bc -l) )); then
+elif (( $(echo "$imc >= 18.5 && $imc <= 24.9" | bc -l) )); then
     status="Peso normal"
 elif (( $(echo "$imc >= 25 && $imc <= 29.9" | bc -l) )); then
     status="Sobrepeso"
@@ -15,7 +15,7 @@ else
     status="Obesidade"
 fi
 
-# clear
+clear # Limpa o terminal
 echo "Resultado:"
-echo "Seu IMC e: $imc" | bc
-echo "Voce esta $status"
+echo "Seu IMC é: $imc"
+echo "Você está: $status"
